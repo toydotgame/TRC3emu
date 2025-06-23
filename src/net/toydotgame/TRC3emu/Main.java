be5a584f8ddc4922e1b59b6d6d2bd6e6deaaad51
toydotgame <toydotgame@gmail.com>
2025-06-24 02:19:18 +1000

    File writing

--- src/net/toydotgame/TRC3emu/Main.java
+++ src/net/toydotgame/TRC3emu/Main.java
@@ -2,6 +2,8 @@ package net.toydotgame.TRC3emu;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -13,6 +15,7 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.OptionGroup;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import net.toydotgame.io.FlushedFileWriter;
 
 public class Main {
 	// Cmdline args yield these settings
@@ -21,6 +24,7 @@ public class Main {
 	public static int mode = -1;
 	public static boolean verbose = false;
 	private static String inputPath;
+	private static String outputPath;
 	
 	public static void main(String[] args) {
 		// Setup options and check them immediately, storing values into variables:
@@ -62,10 +66,22 @@ public class Main {
 		mode.addOption(emulate);
 		mode.setRequired(true);
 		
-		Option verbose = new Option("v", "verbose", false, "Print detailed step-by-step information.");
+		Option verbose = new Option("v",
+			"verbose",
+			false,
+			"Print detailed step-by-step information."
+		);
+		
+		Option output = Option.builder("o")
+			.longOpt("output")
+			.desc("(Optional) Output binary file. This option is ignored when -a, --assemble is not set. Defaults to a .bin of the same name as the input source file.")
+			.hasArg()
+			.argName("destination")
+			.build();
 		
 		options.addOptionGroup(mode);
 		options.addOption(verbose);
+		options.addOption(output);
 		return options;
 	}
 	
@@ -77,6 +93,11 @@ public class Main {
 			if(cmdline.hasOption("a")) {
 				mode = ASSEMBLE;
 				inputPath = cmdline.getOptionValue("a");
+				
+				if(cmdline.hasOption("o"))
+					outputPath = cmdline.getOptionValue("0");
+				else
+					outputPath = inputPath.split("\\.", 2)[0] + ".bin";
 			} else if(cmdline.hasOption("e")) {
 				mode = EMULATE;
 				inputPath = cmdline.getOptionValue("e");
@@ -104,16 +125,12 @@ public class Main {
 		try {
 			input = new Scanner(inputFile);
 		} catch(FileNotFoundException e) {
-			System.err.println("\nCouldn't find file \"" + inputPath + "\"!");
+			System.err.println("\nCouldn't find file \"" + inputFile.getName() + "\"!");
 			System.exit(1);
 		}
 		
 		// Read file into String List
-		List<String> lines = new ArrayList<String>();
-		while(input.hasNextLine()) {
-			String line = input.nextLine().replaceAll("\\s+", " ").trim(); // Lint lines to be kind
-			lines.add(line); // Here, each `line` String is an instruction with its arguments separated by spaces
-		}
+		List<String> lines = readInput(input);
 		
 		System.out.println(" Done!");
 		
@@ -122,7 +139,6 @@ public class Main {
 		Utils.verboseLog(lines.size() + " lines read => " + instructionList.size() + " instructions.");
 		
 		Utils.printAssembly(lines, instructionList); // Won't print without -v
-			
 		
 		Utils.verboseLog("Checking compiled binary alignment...");
 		Utils.checkBinary(instructionList); // Raises syntaxErrors count
@@ -143,7 +159,43 @@ public class Main {
 		} else if(length > 2048)
 			System.err.println("This binary exceeds the maximum space available in TRC3's RAM! You will not be able to run this in Minecraft.");
 		
-		// TODO: File output
+		// Write to output file:
+		System.out.println("Preparing to write...");
+		FlushedFileWriter output;
+		try {
+			output = setupOutput();
+			System.out.print("Output setup done! Writing...");
+			writeOutput(instructionList, output);
+			System.out.println(" Done!");
+		} catch (IOException e) {
+			System.err.println("\nError writing to \"" + outputPath + "\"!");
+			System.exit(2);
+		}
+	}
+	
+	private static List<String> readInput(Scanner scanner) {
+		List<String> fileStream = new ArrayList<String>();
+		while(scanner.hasNextLine()) {
+			String line = scanner.nextLine().replaceAll("\\s+", " ").trim(); // Lint lines to be kind
+			fileStream.add(line); // Here, each `line` String is an instruction with its arguments separated by spaces
+		}
+		
+		return fileStream;
+	}
+	
+	private static FlushedFileWriter setupOutput() throws IOException {
+		File outputFile = new File(outputPath);
+		if(outputFile.createNewFile())
+			Utils.verboseLog("\tWriting to " + outputFile.getName() + ".");
+		else
+			System.err.println("\t" + outputFile.getName() + " already exists! It will be overwritten");
+
+		return new FlushedFileWriter(outputFile);
+	}
+	
+	private static void writeOutput(List<String> byteStream, FlushedFileWriter output) throws IOException {
+		for(String line : byteStream)
+			output.writeln(line);
 	}
 	
 	private static void emulate() {
